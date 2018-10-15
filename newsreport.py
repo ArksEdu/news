89d21f3b056370209ce97b3353e5b45a91e8bced
ArksEdu <arks.edu@gmail.com>
2018-10-15 18:29:30 +1100

    Added error handling and combined common actions into single function

--- newsreport.py
+++ newsreport.py
@@ -9,10 +9,21 @@ AUTHOR_RESULT = '%s - %s views\n'
 ERROR_RESULT = '%s - %s%% errors\n'
 
 
+def connectToDb():
+    try:
+        db = psycopg2.connect(database=DBNAME)
+        cursor = db.cursor()
+        return db, cursor
+    except psycopg2.DatabaseError, e:
+        print("Error occurred in connecting to database.")
+        return None, None
+
+
 def get_topArticles():
     """Return 3 articles, which were the most viewed based on the log."""
-    conn = psycopg2.connect(database=DBNAME)
-    cursor = conn.cursor()
+    conn, cursor = connectToDb()
+    if conn is None or cursor is None:
+        return ""
     selectQuery = """select articles.title, COUNT(title) as num
                     from log
                     join articles on log.path = ('/article/' || articles.slug)
@@ -25,9 +36,11 @@ def get_topArticles():
 
 
 def get_topAuthors():
-    """Return all authors, in order of popularity, detailing the number of views per author."""
-    conn = psycopg2.connect(database=DBNAME)
-    cursor = conn.cursor()
+    """Return all authors, in order of popularity,
+    detailing the number of views per author."""
+    conn, cursor = connectToDb()
+    if conn is None or cursor is None:
+        return ""
     selectQuery = """select authors.name, COUNT(authors.name) as num
                     from log
                     join articles on log.path = ('/article/' || articles.slug)
@@ -42,8 +55,9 @@ def get_topAuthors():
 
 def get_errorPercentages():
     """Return days on which more than 1% of the views resulted in error."""
-    conn = psycopg2.connect(database=DBNAME)
-    cursor = conn.cursor()
+    conn, cursor = connectToDb()
+    if conn is None or cursor is None:
+        return ""
     selectQuery = """select time::date,
     ((COUNT(1) filter (where status = '404 NOT FOUND')) * 100.00 / COUNT(1))
     from log group by time::date
@@ -61,11 +75,12 @@ def get_errorPercentages():
     return errors
 
 
-print('1. What are the most popular three articles of all time?')
-print(get_topArticles())
+if __name__ == "__main__":
+    print("1. What are the most popular three articles of all time?")
+    print(get_topArticles())
 
-print("2. Who are the most popular article authors of all time?")
-print(get_topAuthors())
+    print("2. Who are the most popular article authors of all time?")
+    print(get_topAuthors())
 
-print("3. On which days did more than 1% of requests lead to errors?")
-print(get_errorPercentages())
+    print("3. On which days did more than 1% of requests lead to errors?")
+    print(get_errorPercentages())
